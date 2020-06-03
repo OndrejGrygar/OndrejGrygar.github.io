98d74935eb1354cffcaf279c5737c9b86ad04dfa
OndrejGrygar <61903826+OndrejGrygar@users.noreply.github.com>
2020-06-03 13:10:41 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -14,6 +14,18 @@ $(document).ready(function() {
         autoplayTimeout: 4000,
         dots: false,
         nav: true,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: true
+            },
+            750: {
+                items: 3,
+                nav: true
+            },
+
+        },
         navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')],
     });
 });
